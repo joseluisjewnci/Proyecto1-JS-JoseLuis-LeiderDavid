dc1f0e151fb2ec88eb0973806fd36e15cf4adfc3
joseluisjewnci <josearmandocm1234@gmail.com>
2026-04-10 22:51:05 -0500

    feat: multiplicar notas con map

--- app.js
+++ app.js
@@ -7,3 +7,17 @@ const aprendices = [
 ];
 
 console.log("Sistema iniciado");
+function calcularPromedio() {
+  const suma = aprendices.reduce((acc, aprendiz) => acc + aprendiz.nota, 0);
+  const promedio = suma / aprendices.length;
+
+  console.log("Promedio:", promedio);
+}
+calcularPromedio();
+
+function multiplicarNotas() {
+  const resultado = aprendices.map(aprendiz => aprendiz.nota * 2);
+  console.log("Notas multiplicadas:", resultado);
+}
+
+multiplicarNotas();
