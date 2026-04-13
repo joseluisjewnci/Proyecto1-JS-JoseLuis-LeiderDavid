d2d10ff2a76a524bdccac351baa578d8bc310ed8
joseluisjewnci <josearmandocm1234@gmail.com>
2026-04-12 22:42:56 -0500

    guardo cambios

--- app.js
+++ app.js
@@ -22,14 +22,15 @@ function preguntar(texto) {
 async function menu() {
   let opcion = "";
 
-  while (opcion !== "6") {
+  while (opcion !== "7") {
     opcion = await preguntar(`
 1. Promedio
-2. Multiplicar notas
+2. Nombres en mayúsculas
 3. Mostrar aprendices
 4. Aprobados
-5. Ordenar
-6. Salir
+5. Reprobados
+6. Ordenar
+7. Salir
 Elige una opción: `);
 
     switch (opcion) {
@@ -37,18 +38,21 @@ Elige una opción: `);
         console.log("Función promedio pendiente");
         break;
       case "2":
-        console.log("Función multiplicar pendiente");
+        console.log("Función map pendiente");
         break;
       case "3":
-        console.log(aprendices);
+        console.log("Función mostrar aprendices pendiente");
         break;
       case "4":
         console.log("Función aprobados pendiente");
         break;
       case "5":
-        console.log("Función ordenar pendiente");
+        console.log("Función reprobados pendiente");
         break;
       case "6":
+        console.log("Función ordenar pendiente");
+        break;
+      case "7":
         console.log("Saliendo...");
         rl.close();
         break;
