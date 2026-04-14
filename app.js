0fc3c786d6b0ef18cb14d4ce4e616961f31ea2a1
joseluisjewnci <josearmandocm1234@gmail.com>
2026-04-13 21:24:35 -0500

    guardo funcion mostrar reprobados

---
+++ app.js
@@ -0,0 +1,17 @@
+const aprendices = [
+  { nombre: "Ana", nota: 4.5, programa: "ADSO" },
+  { nombre: "Luis", nota: 2.8, programa: "ADSO" },
+  { nombre: "Marta", nota: 3.7, programa: "Diseño Web" },
+  { nombre: "Pedro", nota: 1.9, programa: "ADSO" },
+  { nombre: "Sofia", nota: 5.0, programa: "Diseño Web" }
+];
+
+console.log("Sistema iniciado");
+
+function mostrarReprobados(){
+const reprobados = aprendices.filter(a => a.nota <3);
+console.log("aprendices reprobados: ");
+reprobados.forEach(a =>{
+console.log(`${a.nombre}-${a.nota}`);
+});
+}
