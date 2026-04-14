864ea139c458a4e85900ed1a0b7f78866633859f
LeiderDavidRinconesAltahona <rinconestec1028@gmail.com>
2026-04-13 22:18:03 -0500

    problema

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
 
 
-<<<<<<< HEAD
-=======
-function calcularPromedio(){
-if (aprendices.length === 0){console.log("No hay aprendices");
-return;
-}
-const suma = aprendices.reduce((acc, a)=> acc + a.nota, 0);
-const promedio = suma / aprendices.length;
 
-console.log("promedio:", promedio.toFixed(2));
-}
-
->>>>>>> 5b3165c6b67e37ba15594025c314e234e77e843a
