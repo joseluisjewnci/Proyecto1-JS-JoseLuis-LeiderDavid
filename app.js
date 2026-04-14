b45179c92e81d4f73517abbcc1088cd2898f511f
LeiderDavidRinconesAltahona <rinconestec1028@gmail.com>
2026-04-13 22:07:35 -0500

    cambios

--- app.js
+++ app.js
@@ -1,21 +1,2 @@
-const aprendices = [
-  { nombre: "Ana", nota: 4.5, programa: "ADSO" },
-  { nombre: "Luis", nota: 2.8, programa: "ADSO" },
-  { nombre: "Marta", nota: 3.7, programa: "Diseño Web" },
-  { nombre: "Pedro", nota: 1.9, programa: "ADSO" },
-  { nombre: "Sofia", nota: 5.0, programa: "Diseño Web" }
-];
 
-console.log("Sistema iniciado");
 
-function calcularPromedio(){
-if (aprendices.length === 0){console.log("No hay aprendices");
-return;
-}
-const suma = aprendices.reduce((acc, a)=> acc + a.nota, 0);
-const promedio = suma / aprendices.length;
-
-console.log("promedio:", promedio.toFixed(2));
-}
-
-calcularPromedio();
