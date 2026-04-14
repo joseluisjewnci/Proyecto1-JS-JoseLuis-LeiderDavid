d2c14f9b2ab755e8912b56eeb527877392d8b86d
LeiderDavidRinconesAltahona <rinconestec1028@gmail.com>
2026-04-13 21:45:26 -0500

    sort de notas

---
+++ app.js
@@ -0,0 +1,22 @@
+const aprendices = [
+  { nombre: "Ana", nota: 4.5, programa: "ADSO" },
+  { nombre: "Luis", nota: 2.8, programa: "ADSO" },
+  { nombre: "Marta", nota: 3.7, programa: "Diseño Web" },
+  { nombre: "Pedro", nota: 1.9, programa: "ADSO" },
+  { nombre: "Sofia", nota: 5.0, programa: "Diseño Web" }
+];
+
+function ordenarNotas(){
+  const ordenados = [...aprendices].sort((a,b)=> b.nota-a.nota);
+
+  console.log("Aprendices ordenados de mayor a menor:");
+  ordenados.forEach(a=>{console.log( '${ a.nombre} - $ {a.nota}'S);
+
+  });}
+
+  ordenarNotas();
+
+
+
+
+
